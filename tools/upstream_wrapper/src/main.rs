28c1bd37d44376c36947753cbcd044ec212d53a9
Peter Kolloch <info@eigenvalue.net>
2024-06-18 19:02:54 +0200

    upstream_wrapper: Generalize upstream_wrapper for rustfmt
    ...so we can reuse it for more upstream binaries

--- tools/upstream_wrapper/src/main.rs
+++ tools/upstream_wrapper/src/main.rs
@@ -1,23 +1,29 @@
 use std::path::PathBuf;
 use std::process::{exit, Command};
 
+const WRAPPED_TOOL_NAME: &str = env!("WRAPPED_TOOL_NAME");
+const WRAPPED_TOOL_TARGET: &str = env!("WRAPPED_TOOL_TARGET");
+
 fn main() {
     let runfiles = runfiles::Runfiles::create().unwrap();
 
-    let rustfmt = runfiles::rlocation!(runfiles, env!("RUSTFMT"));
-    if !rustfmt.exists() {
-        panic!("rustfmt does not exist at: {}", rustfmt.display());
+    let wrapped_tool_path = runfiles::rlocation!(runfiles, WRAPPED_TOOL_TARGET);
+    if !wrapped_tool_path.exists() {
+        panic!(
+            "{WRAPPED_TOOL_NAME} does not exist at: {}",
+            wrapped_tool_path.display()
+        );
     }
 
     let working_directory = std::env::var_os("BUILD_WORKING_DIRECTORY")
         .map(PathBuf::from)
         .unwrap_or_else(|| std::env::current_dir().expect("Failed to get working directory"));
 
-    let status = Command::new(rustfmt)
+    let status = Command::new(wrapped_tool_path)
         .current_dir(&working_directory)
         .args(std::env::args_os().skip(1))
         .status()
-        .expect("Failed to run rustfmt");
+        .unwrap_or_else(|e| panic!("Failed to run {WRAPPED_TOOL_NAME} {:#}", e));
     if let Some(exit_code) = status.code() {
         exit(exit_code);
     }
