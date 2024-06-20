ad0d8900fe9431c710d04fbf96d72faf46194003
Peter Kolloch <info@eigenvalue.net>
2024-06-20 20:14:38 +0200

    upstream_wrapper: correct clippy suggestion unwrap_or_default

--- tools/upstream_wrapper/src/main.rs
+++ tools/upstream_wrapper/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     }
 
     let tool_directory = wrapped_tool_path.parent().expect("parent directory of tool binary");
-    let old_path = std::env::var_os("PATH").unwrap_or(OsString::new());
+    let old_path = std::env::var_os("PATH").unwrap_or_default();
     let mut new_path = OsString::from(tool_directory);
     new_path.push(PATH_SEPARATOR);
     new_path.push(&old_path);
